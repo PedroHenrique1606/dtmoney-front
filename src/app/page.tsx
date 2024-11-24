456e687139c934211f3e559cf28ff269af64fffc
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2024-11-24 15:19:38 -0300

    feat: add accordion from benefits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,59 @@ import WordRotate from "@/components/ui/word-rotate";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SafariDemo } from "@/components/SafariDemo";
-import HorizontalCard from "@/components/HorizontalCard";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Image from "next/image";
+import HomeDesktop from "@/assets/home-desktop.svg";
+
+const beneficios = [
+  {
+    id: "item-1",
+    pergunta: "O dtmoney é fácil de usar?",
+    resposta:
+      "Sim. O dtmoney possui uma interface intuitiva e fácil de navegar.",
+  },
+  {
+    id: "item-2",
+    pergunta: "Posso cadastrar minhas transações de forma rápida?",
+    resposta:
+      "Sim. O dtmoney permite o cadastro rápido de transações com apenas alguns cliques.",
+  },
+  {
+    id: "item-3",
+    pergunta: "O dtmoney é seguro?",
+    resposta:
+      "Sim. Utilizamos autenticação segura e proteção dos seus dados financeiros.",
+  },
+  {
+    id: "item-4",
+    pergunta: "É possível visualizar relatórios financeiros?",
+    resposta:
+      "Sim. O dtmoney oferece relatórios financeiros detalhados para análise de gastos.",
+  },
+  {
+    id: "item-5",
+    pergunta: "O dtmoney é gratuito?",
+    resposta: "Sim. O dtmoney é totalmente gratuito para uso pessoal.",
+  },
+  {
+    id: "item-6",
+    pergunta: "Posso acessar o dtmoney de qualquer dispositivo?",
+    resposta:
+      "Sim. O dtmoney é responsivo e funciona em qualquer dispositivo, seja desktop ou mobile.",
+  },
+];
 
 const Home = () => {
   return (
     <div className="bg-gray1 text-gray6 min-h-screen flex flex-col">
       {/* Hero Section */}
       <Navbar />
-      <header className="text-white py-16 px-8">
+      <header className="text-white py-16 px-8 mt-20">
         <div
           className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between 
           motion-safe:animate-fade-in-down"
@@ -45,60 +90,29 @@ const Home = () => {
       {/* Features Section */}
       <section className="py-16 px-8 bg-gray1 space-y-10">
         <div
-          className="max-w-7xl mx-auto text-center 
+          className="max-w-7xl mx-auto
           motion-safe:animate-fade-in-up"
         >
           <SafariDemo />
-          <h2 className="text-3xl md:text-4xl font-bold text-gray7 mt-20">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray7 mt-20 text-center ">
             Por que escolher o DT Money?
           </h2>
-          <p className="mt-4 text-gray6 mb-20">
+          <p className="mt-4 text-gray6 mb-20 text-center ">
             Oferecemos as melhores funcionalidades para você alcançar sua
             independência financeira.
           </p>
-          <div className="flex flex-col space-y-16 items-center justify-center">
-            <HorizontalCard
-              imageSrcOne="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Gerencie suas finanças"
-              description="Tenha controle total dos seus gastos, prioridades e projeção de gastos"
-              link="#"
-              imageSrcTwo=""
-            />
-            <HorizontalCard
-              imageSrcOne=""
-              imageSrcTwo="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Planeje seus gastos"
-              description="Saiba gerenciar cada gasto com base nos gastos anteriores"
-              link="#"
-            />
-            <HorizontalCard
-              imageSrcOne="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Gerencie suas finanças"
-              description="Tenha controle total dos seus gastos, prioridades e projeção de gastos"
-              link="#"
-              imageSrcTwo=""
-            />
-            <HorizontalCard
-              imageSrcOne=""
-              imageSrcTwo="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Planeje seus gastos"
-              description="Saiba gerenciar cada gasto com base nos gastos anteriores"
-              link="#"
-            />
-            <HorizontalCard
-              imageSrcOne="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Gerencie suas finanças"
-              description="Tenha controle total dos seus gastos, prioridades e projeção de gastos"
-              link="#"
-              imageSrcTwo=""
-            />
-            <HorizontalCard
-              imageSrcOne=""
-              imageSrcTwo="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg/1200px-2020-03-19_Entrevista_concedida_ao_apresentador_Ratinho_do_SBT_21_%28cropped%29.jpg"
-              title="Planeje seus gastos"
-              description="Saiba gerenciar cada gasto com base nos gastos anteriores"
-              link="#"
-            />
+          <div className="flex gap-8 space-y-16">
+            <div>
+              <Image src={HomeDesktop} alt="Home" />
+            </div>
+            <Accordion type="single" collapsible className="w-full">
+              {beneficios.map((beneficio) => (
+                <AccordionItem key={beneficio.id} value={beneficio.id}>
+                  <AccordionTrigger>{beneficio.pergunta}</AccordionTrigger>
+                  <AccordionContent>{beneficio.resposta}</AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
           </div>
         </div>
       </section>
